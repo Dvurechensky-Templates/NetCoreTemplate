2c35a5d1d57e9d135378e5162cd8c1aaa54f486d
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-10-13 10:03:20 +0300

    Auto-update headers via pre_commit.ps1 - 2025-10-13

--- APP_LOGGING/Accessories/LoggingAccessories/LoggingExtensions.cs
+++ APP_LOGGING/Accessories/LoggingAccessories/LoggingExtensions.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 12 октября 2025 10:59:58
- * Version: 1.0.52
+ * Last Updated: 13 октября 2025 10:03:19
+ * Version: 1.0.53
  */
 
 using System.Reflection;
