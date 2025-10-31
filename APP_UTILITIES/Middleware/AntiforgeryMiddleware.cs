982cdabd9c5c8c487c74992d53a373360fcd04a5
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-10-31 06:53:05 +0300

    Auto-update headers via pre_commit.ps1 - 2025-10-31

--- APP_UTILITIES/Middleware/AntiforgeryMiddleware.cs
+++ APP_UTILITIES/Middleware/AntiforgeryMiddleware.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 30 октября 2025 17:08:17
- * Version: 1.0.70
+ * Last Updated: 31 октября 2025 06:53:04
+ * Version: 1.0.71
  */
 
 using APP_LOGGING.Accessories.LoggingAccessories;
