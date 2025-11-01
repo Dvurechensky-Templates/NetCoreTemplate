5393a7aa03bd8585603bce6bdde615a3cd649a48
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-11-01 06:52:59 +0300

    Auto-update headers via pre_commit.ps1 - 2025-11-01

--- APP_UTILITIES/Accessories/JsonAccessories/JsonExtensions.cs
+++ APP_UTILITIES/Accessories/JsonAccessories/JsonExtensions.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 31 октября 2025 06:53:04
- * Version: 1.0.71
+ * Last Updated: 01 ноября 2025 06:52:58
+ * Version: 1.0.72
  */
 
 using System.Text.Json;
