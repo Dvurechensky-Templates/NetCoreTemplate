b444ad937259c5c05b43ed135e94833e815860c8
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-12-15 06:52:59 +0300

    Auto-update headers via pre_commit.ps1 - 2025-12-15

--- APP_UTILITIES/FormatsData/AppResponseData/BadResponses/FailedRequestReason.cs
+++ APP_UTILITIES/FormatsData/AppResponseData/BadResponses/FailedRequestReason.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 14 декабря 2025 14:35:08
- * Version: 1.0.115
+ * Last Updated: 15 декабря 2025 06:52:58
+ * Version: 1.0.116
  */
 
 using System.Text.Json.Serialization;
