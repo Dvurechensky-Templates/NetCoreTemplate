88bccb40ed91fc6acb4f97493dd56b10b3e2e9ea
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-01-13 13:17:52 +0300

    Auto-update headers via pre_commit.ps1 - 2026-01-13

--- APP_WEB/Models/ErrorViewModel.cs
+++ APP_WEB/Models/ErrorViewModel.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 12 января 2026 06:53:05
- * Version: 1.0.144
+ * Last Updated: 13 января 2026 13:17:51
+ * Version: 1.0.145
  */
 
 namespace AppWeb.Models
