fc0cc0a6891221995516a96c6052f523fabe4e01
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-01 15:07:50 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-01

--- APP_WEB/Controllers/HomeController.cs
+++ APP_WEB/Controllers/HomeController.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 31 мая 2026 15:11:29
- * Version: 1.0.284
+ * Last Updated: 01 июня 2026 15:07:49
+ * Version: 1.0.285
  */
 
 using System.Diagnostics;
