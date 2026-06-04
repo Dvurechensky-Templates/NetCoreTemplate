ba717a8696fd343d04a31ee140354464ec7b8de5
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-04 07:13:18 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-04

--- APP_LOGGING/Services/AppLoggingService/Implements/LoggingService.cs
+++ APP_LOGGING/Services/AppLoggingService/Implements/LoggingService.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 03 июня 2026 15:37:59
- * Version: 1.0.287
+ * Last Updated: 04 июня 2026 07:13:17
+ * Version: 1.0.288
  */
 
 using System.Text;
