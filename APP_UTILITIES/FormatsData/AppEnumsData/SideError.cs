6ef4ea1f21ad0a2f5c7056b7eece43ae70567642
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-15 07:13:30 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-15

--- APP_UTILITIES/FormatsData/AppEnumsData/SideError.cs
+++ APP_UTILITIES/FormatsData/AppEnumsData/SideError.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 14 июня 2026 16:53:57
- * Version: 1.0.298
+ * Last Updated: 15 июня 2026 07:13:28
+ * Version: 1.0.299
  */
 
 namespace APP_UTILITIES.FormatsData.AppEnumsData;
